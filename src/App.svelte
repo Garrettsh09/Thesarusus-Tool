f6bced39d2e3476f72df4b4e5191247377976b9a
garrettsh09 <sharpeg68@gmail.com>
2023-04-13 09:23:22 -0400

    Changes

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { Col, Container, Row } from 'sveltestrap';
+    import Generate from './GenerateButton.svelte';
+</script>
+
+<style>
+	:global(main){
+	  font-family: monospace;
+	  text-align: center;
+	  color: green;
+	  background-color: rgb(194, 184, 173);
+	  font-size: 1em;
+	}
+</style>
+
+<main>
+	<Container>
+		<Row>
+			<Col>
+				<h1>Thesarusus Tool</h1>
+			</Col>
+		</Row>
+		<Generate/>
+	</Container>
+</main>
+
---
+++ src/GenerateButton.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { Form, FormGroup, FormText, Input, Label, Col, Row, Button } from 'sveltestrap';
+</script>
+
+<Form>
+    <Row>
+        <Col>
+            <FormGroup>
+                <Input type="text" id="Generate" placeholder="Put your scentence here"/>
+                <h1> </h1>
+                <Button color="primary" class="Generate">Generate</Button>
+            </FormGroup>
+        </Col>
+    </Row>
+</Form>
